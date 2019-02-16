a30bc425ed92310aba41906c4170399f56a5fd40
Stuart Nevans Locke <mc12stoud@gmail.com>
2019-02-16 13:39:15 -0500

    Add some move funcs

--- move.h
+++ move.h
@@ -1,8 +1,12 @@
 #include "types.h"
 
 //The move structure looks like the following.
-//
-//
+//bits 0-5  (move start)
+//bits 6-11 (move dest)
+//bits 12-14 (PieceType moved)
+//bits 15-16 (MoveType type)
+//bits 17-19 (PieceType promote)
+
 
 typedef U32 Move;
 
@@ -10,13 +14,33 @@ enum MoveType{
     NORMAL=0,
     PROMOTION=1,
     CASTLE=2,
-    ENPASSANT
+    ENPASSANT=3
 }
 
 constexpr Move makeMove(U8 from, U8 to, PieceType pieceMoved){
-    return (pieceMoved<<16)+(from<<8)+to;
+    return (pieceMoved<<12)+(from<<6)+to;
 }
 
 constexpr Move makeMove(U8 from, U8 to, PieceType pieceMoved, MoveType moveType, PieceType promoteTo){
-    return moveType<<+(pieceMoved<<16)+(from<<8)+to;
+    return (promoteTo<<17)+(moveType<<15)+(pieceMoved<<16)+(from<<8)+to;
+}
+
+constexpr U8 dest_sq(Move move){
+    return (move>>6)&7;
+}
+
+constexpr U8 to_sq(Move move){
+    return move & 7;
+}
+
+constexpr PieceType get_piece_moved(Move move){
+    return PieceType((move>>12)&7);
+}
+
+constexpr MoveType get_move_type(Move move){
+    return MoveType((move>>15)&3);
+}
+
+constexpr PieceType get_promotion_type(Move move){
+    return PieceType((move>>17)&7);
 }
