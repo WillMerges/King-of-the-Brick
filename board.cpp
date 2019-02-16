70d1bfd2877153fb9253649a04c1f81b636cb485
Kenny Kim <kennykim11@gmail.com>
2019-02-16 16:22:50 -0500

    even more board stuff

--- board.cpp
+++ board.cpp
@@ -30,14 +30,39 @@ Position * Board::getCurPos(){
 }
 
 void Board::makeMove(Move move){
+    //Setting up some local variables to make things easier later on
     PieceType pieceToMove = get_piece_moved(move);
     U8 from = from_sq(move);
     U8 to = to_sq(move);
-
     Bitboard * moverPieces = pos->whiteToMove ? pos->whitePieces : pos->blackPieces; //getting the pieces of who is moving
+    Bitboard * opponentPieces = pos->whiteToMove ? pos->blackPieces : pos->whitePieces; //getting the pieces of who is not moving
+    MoveType moveType = get_move_type(move);
+
+    //Setting up the move
     moverPieces[pieceToMove] =  moverPieces[pieceToMove] & ~squareMasks[from]; //setting the bit of where the moving piece came from to 0
-    if (get_move_type(move) == PROMOTION){
-        
+    
+    for (U8 pieceTypeInd = 0; pieceTypeInd < 6; pieceTypeInd++){ //looping through types of pieces on the opponent side
+        opponentPieces[pieceTypeInd] = opponentPieces[pieceToMove] & ~squareMasks[to]; //setting the bit for that square to 0
+    }
+
+    switch (moveType)
+    {
+        case NORMAL:
+            moverPieces[pieceToMove] =  moverPieces[pieceToMove] | squareMasks[to];
+            break;
+
+        case PROMOTION:
+            PieceType newPiece = get_promotion_type(move);
+            moverPieces[pieceToMove] =  moverPieces[pieceToMove] | squareMasks[to];
+            break;
+
+        case CASTLE:
+            
+            moverPieces[pieceToMove] =  moverPieces[pieceToMove] | squareMasks[to];
+            break;
+
+        default:
+            break;
     }
 }
 
