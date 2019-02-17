1e6719e728ee19880cd1ae9ba6224fc02fd67d9c
Kenny Kim <kennykim11@gmail.com>
2019-02-16 22:43:49 -0500

    search.cpp

--- search.cpp
+++ search.cpp
@@ -1,7 +1,14 @@
 #include "eval.h"
+#include "search.h"
 
 
+int getBestMove(Board * baord, Config * config){
 
+}
+
+int alphaBeta(Board * board, int alpha, int beta, int depth){
+    
+}
 
 //get all legal moves
 //evaluate
