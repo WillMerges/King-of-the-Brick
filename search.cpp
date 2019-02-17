4ec66cb792eeed253c27929d76720d7e62d394f1
Will Merges <wdmerges@gmail.com>
2019-02-16 23:07:45 -0500

    :b::b::b:

--- search.cpp
+++ search.cpp
@@ -1,26 +1,46 @@
 #include "eval.h"
 #include "search.h"
+#include <limits.h>
 
 
-int getBestMove(Board * baord, Config * config){
-
+int getBestMove(Board * board, Config * config){
+    ExtMoves moves[MAX_MOVES];
+    int moveCount = getAllLegalMoves(board, moves);
+    int lowest = INT_MAX;
+    int highest = INT_MIN;
+    Move move_highest;
+    Move move_lowest;
+    for(int i=0; i<moveCount; i++) {
+        int eval = alphaBeta(board, INT_MIN, INT_MAX, 1);
+        if(eval > highest) {
+            highest = eval;
+            move_highest = moves[i];
+        } if(eval < lowest) {
+            lowest = eval;
+            move_lowest = moves[i];
+        }
+        if(board->pos->whiteToMove()) {
+            return move_highest;
+        } else {
+            return move_lowest;
+        }
+    }
 }
 
 int alphaBeta(Board * board, int alpha, int beta, int depth){
-    ExtMove moves[MAX_MOVES];
+    ExtMoves moves[MAX_MOVES];
     int moveCount = getAllLegalMoves(board,moves);
-    int eval = evaluate(board);
+    int eval = evalutate(board);
     if(depth <= 0 || moveCount <= 0){
         return eval;
     }
     //Do null move pruning
-    int val;
     int adjDepth=depth-1;
     bool whiteToMove=board->pos->whiteToMove;
     for(int i = 0; i < moveCount; i++){
         Move move = moves[i].move;
         board->makeMove(move);
-        val = alphaBeta(board,-beta,-alpha,adjDepth);
+        val = -alphaBeta(board,-beta,-alpha,adjDepth);
         board->undoMove();
         if(val > alpha){
             alpha=val;
@@ -32,3 +52,5 @@ int alphaBeta(Board * board, int alpha, int beta, int depth){
     return alpha;
 }
 
+//get all legal moves
+//evaluate
